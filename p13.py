abe3c31d3dc27b0c7404ae804c1a92d77c0fa642
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-08-09 15:41:51 +0530

    commit 4

--- p13.py
+++ p13.py
@@ -1,9 +1,11 @@
-import sys
+
+erro=0
 try:
     h=int(input("Enter hours : "))
     r=int(input("Enter hours : "))
 except ValueError:
     print("Error, please enter numeric input")
-    sys.exit()
-pay=h*r
-print("Pay = ",pay)
+    erro=1
+if erro==0:
+    pay=h*r
+    print("Pay = ",pay)
